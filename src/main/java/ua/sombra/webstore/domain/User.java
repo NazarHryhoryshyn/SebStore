0617e0886ec6e2d9e5677d0714f5d0ba4380a88a
nazik.star1@mail.ru <doggi star>
2017-02-18 15:01:40 +0200

    configured hibernate framework 4

---
+++ src/main/java/ua/sombra/webstore/domain/User.java
@@ -0,0 +1,61 @@
+package ua.sombra.webstore.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue
+	int id;
+	
+	@Column(name="first_name")
+	String firstname;
+	
+	@Column(name="last_name")
+	String lastname;
+	
+	String email;
+	String telephone;
+	int sex;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	
+}
